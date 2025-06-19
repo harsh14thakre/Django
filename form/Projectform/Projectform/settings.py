cebc2b6e3e3f1b18a18f18454826cf2ee9231773
harsh14thakre <harsh14thakre@gmail.com>
2025-06-19 21:37:24 +0530

    commit

--- form/Projectform/Projectform/settings.py
+++ form/Projectform/Projectform/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
 TEMPLATESDIRS = os.path.join(BASE_DIR,'templates')
 # STATIC_DIR=os.path.join(BASE_DIR,'static')
 
