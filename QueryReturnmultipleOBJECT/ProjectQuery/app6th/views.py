76ce56e84966b797faf8ef980f5520c98c84785b
harsh14thakre <harsh14thakre@gmail.com>
2025-06-24 16:47:23 +0530

    commit

--- QueryReturnmultipleOBJECT/ProjectQuery/app6th/views.py
+++ QueryReturnmultipleOBJECT/ProjectQuery/app6th/views.py
@@ -3,8 +3,9 @@ from .models import Student as stu
 
 # Create your views here. 
 def home(req):
-    all_data=stu.objects.filter()
+    # all_data=stu.objects.order_by('-name')
     # print(all_data)
+    all_data=stu.objects.exclude(name="Harsh")
     print(type(all_data))
     for i in all_data:
         print(i.name)
@@ -12,4 +13,6 @@ def home(req):
         print(i.contact)
         print(i.city)
         print(all_data.values_list())
+        
+ 
         
